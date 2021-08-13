b35aa57fa40ff2c9c0b95ec58248c5bedcdc5160
Tim Biddington <timbiddington@fastmail.com.au>
2021-08-13 14:10:37 +1000

    Remove compiled assets from repo

--- dist/src/Crest.d.ts
+++
@@ -1,8 +0,0 @@
-import { Token } from './Token.js';
-export declare class Crest {
-    static tokens: Array<Token>;
-    static hadError: boolean;
-    static run(source: string): void;
-    static error(line: number, message: string): void;
-    static report(line: number, where: string, message: string): void;
-}
--- dist/src/Token.d.ts
+++
@@ -1,9 +0,0 @@
-import { TokenType } from './TokenType.js';
-export declare class Token {
-    type: TokenType;
-    lexeme: string;
-    literal?: any;
-    line: number;
-    constructor(type: TokenType, lexeme: string, line: number, literal?: any);
-    toString(): string;
-}
