47ab821570510ef6707b0488bad0c866ee246567
Tim Biddington <timbiddington@fastmail.com.au>
2021-10-12 20:13:12 +1100

    Change select-op to Segment Implement styles

--- app/ui/Segment.tsx
+++ app/ui/Segment.tsx
@@ -2,10 +2,14 @@ import * as React from 'react';
 import { Op } from '../domain/Types.js';
 
 const Segment = (props) => {
-  return  <div className="select-op">
+  return  <div className="segment">
             { Object.entries(Op).map(([k,v]) => {
-                return <input type="radio" value={v} id={v} className="select-op__op" key={k}
-                              name="op" onChange={e => props.onChange( e.target.value as Op )} checked={props.selected === v} /> })}
+                return  <div className="segment-option">
+                          <input type="radio" value={v} id={v} className="segment-backinginput" key={k}
+                              name="op" onChange={e => props.onChange( e.target.value as Op )} checked={props.selected === v} />
+                          <div className="segment-label">{k}</div>
+                        </div>})
+            }
           </div>
 }
 
